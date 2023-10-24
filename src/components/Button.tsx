60dbb30255cb524961f80b0145f79036f96f084f
Marnien Cueba <marniencueba94@gmail.com>
2023-10-25 01:21:01 +0800

    components

---
+++ src/components/Button.tsx
@@ -0,0 +1,86 @@
+import React from "react";
+import cn from "classnames";
+import { Icon } from "./Icon";
+import { DefaultColorEnum, SizeEnum } from "../shared";
+import { buttonStyles } from "../styles";
+
+type TButtonProps = React.HTMLAttributes<HTMLButtonElement> & {
+  iconPath?: string;
+  iconPosition?: "left" | "right";
+  label?: string;
+  loading?: boolean;
+  disabled?: boolean;
+  layout?: "auto" | "block";
+  type?: "button" | "reset" | "submit";
+  variant?: "solid" | "outline" | "ghost";
+  corner?: "flat" | "soft-edge" | "rounded";
+  size?: keyof typeof SizeEnum;
+  color?: keyof typeof DefaultColorEnum;
+};
+
+export const Button: React.FC<TButtonProps> = (props) => {
+  const {
+    className,
+    iconPath,
+    label,
+    children,
+    id = "button",
+    size = "md",
+    layout = "auto",
+    type = "button",
+    color = "primary",
+    variant = "solid",
+    corner = "soft-edge",
+    iconPosition = "left",
+    disabled = false,
+    loading = false,
+    ...restProps
+  } = props;
+
+  const { root } = buttonStyles;
+
+  const baseClasses = cn(
+    "cm-button",
+    root.base,
+    root.sizes[size],
+    root.corners[corner],
+    root.variants[variant].colors[color].base,
+    disabled || loading
+      ? [root.cursors.disable, root.variants[variant].colors[color].disable]
+      : [root.cursors.default, root.variants[variant].colors[color].default],
+    {
+      [root.block.base]: layout === "block",
+      [root.widthSizes[size]]: !(label || children),
+    },
+    className
+  );
+
+  return (
+    <button
+      {...restProps}
+      id={id}
+      type={type}
+      className={baseClasses}
+      aria-disabled={disabled || loading}
+      aria-label={label}
+    >
+      {iconPosition === "left" && iconPath && (
+        <Icon
+          path={iconPath}
+          color={color}
+          className={cn({ [root.icon.base]: loading })}
+        />
+      )}
+
+      {loading ? "Loading..." : label || children}
+
+      {iconPosition === "right" && iconPath && (
+        <Icon
+          path={iconPath}
+          color={color}
+          className={cn({ [root.icon.base]: loading })}
+        />
+      )}
+    </button>
+  );
+};
--- src/components/Icon.tsx
+++ src/components/Icon.tsx
@@ -1,28 +1,6 @@
 import React from "react";
 import cn from "classnames";
-
-const iconStyles = {
-  root: {
-    base: "",
-    size: {
-      sm: "w-5 h-5",
-      md: "w-6 h-6",
-      lg: "w-8 h-8",
-      xl: "w-10 h-10",
-    },
-    color: {
-      primary: "text-primary",
-      secondary: "text-secondary",
-      info: "text-info",
-      success: "text-success",
-      warning: "text-warning",
-      danger: "text-danger",
-      default: "text-default",
-      black: "text-black",
-      white: "text-white",
-    },
-  },
-};
+import { iconStyles } from "../styles";
 
 export type IconProps = {
   className?: string;
